109b7c241a899e966c7e212e1842c6644b844acd
Cesar Menegatti <cesinha51@gmail.com>
2020-04-08 22:11:40 -0300

    Desafio Concluído

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid } = require('uuidv4');
 
 // const { uuid } = require("uuidv4");
 
@@ -11,23 +12,90 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = { 
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return response.status(200).json(repository);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repoIndex = repositories.findIndex(repos => repos.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: 'Repository does not exist!' });
+  }
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repoIndex].likes,
+  };
+
+  repositories[repoIndex] = repository;
+
+  return response.json(repository);
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex(repos => repos.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: 'Repository does not exist!' });
+  }
+
+  repositories.splice(repoIndex, 1);
+
+  return response.status(204).send('');
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex(repos => repos.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: 'You can not like an inexistent repository! '})
+  }
+  
+  const { title, url, techs, likes } = repositories[repoIndex];
+
+  let newLike = likes + 1;
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: newLike,
+  };
+
+  repositories.push(repository)
+
+  repositories.splice(repoIndex, 1);
+
+  return response.json(repository);
+
 });
 
 module.exports = app;
